d694013cabf60fe5eaee4afa5ab0601818448e2a
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-01-29 14:27:41 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+from codecs import open
+import os
+
+current_path = os.getcwd()
+
+with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='simple_othello', 
+    packages=find_packages(exclude=('*.pyc',)),
+    version='0.0.0',
+    license='MIT', 
+    install_requires=[],
+    author='Michiyasu Uchiyama',
+    description='simple_othello', 
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    keywords='simple_othello othello reversi', 
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
