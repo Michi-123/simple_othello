939ab645fb2acba9684c914fe97763310bdf90d9
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-01-29 18:32:58 +0900

    Add replace()

--- simple_othello/humanPlayer.py
+++ simple_othello/humanPlayer.py
@@ -9,11 +9,12 @@ class PlayerHuman(Agent):
 
     def get_pos(self):
         print("player", self.env.player)
-        pos = input()
+        pos = input('(x,y):')
 
         if pos is None or pos == "":
             pos = 0
         else:
+            pos = pos.replace('.', ',')
             pos = pos.split(',')
             pos = int(pos[0]) * self.CFG.lines + int(pos[1])
             print("pos", pos)
