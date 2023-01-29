d6e6fd7ca7aca207c98ec37cee229bcdba5e1274
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-01-29 18:31:02 +0900

    Update othello.py

--- simple_othello/othello.py
+++ simple_othello/othello.py
@@ -43,20 +43,26 @@ class Othello:
 
     def _set_init_state(self):
         state = []
-        for i in range(self.CFG.action_size):
-            state.append(self.CFG.blank)
+        
+        for r in range(self.CFG.lines):
+            row = []
+
+            for c in range(self.CFG.lines):
+                row.append(self.CFG.blank)
+
+            state.append(row)
             
         # 初期ピース
-        state[27] = 1
-        state[28] = -1
-        state[35] = -1
-        state[36] = 1
+        state[3][3] = 1
+        state[3][4] = -1
+        state[4][3] = -1
+        state[4][4] = 1
+
         self.state = state
 
     def step(self, action):
         reward = 0
         done = False
-
 
         if self._is_done():
             done = True
@@ -69,58 +75,57 @@ class Othello:
             return self.state, reward, done
 
         if action == self.CFG.pass_:
-            print("Pass");input("Wait...")
+            print("Pass")
             self.player = -self.player
             return self.state, reward, done
 
         if not self._is_legal_action(action):
-            input("E100..not legal action!....")
+            input("E100: Not legal action.")
+            self.player = -self.player
             return self.state, reward, done
 
-        # change_pos_list = self.get_change_pos_list(action)
-
         for pos in self.change_pos_list:
-            self.state[pos] = self.player
+            x,y = self._action2xy(pos)
+            # self.state[pos] = self.player
+            self.state[x][y] = self.player
 
         next_state = self._get_next_state(action)
         self.state = next_state
-
-        # if self._is_done():
-        #     done = True
- 
-        #     if not self._is_draw():
-        #         reward = -1
-            
-        #     return next_state, reward, done
             
         self.player = -self.player
         return next_state, reward, done
 
     def render(self, plt=None):
-        state = np.array(self.state).reshape(self.CFG.lines, -1)
 
         if plt==True:
-            # print(state)
             self.draw_board()
         else:
-            print(state)
+            print(self.state)
 
     def _get_next_state(self, action):
         next_state=copy.deepcopy(self.state)
-        next_state[action] = self.player
+        x,y = self._action2xy(action)
+        next_state[x][y] = self.player
         return next_state
 
+    def _matrix2vec(self, state):
+        state_vec = np.array(state).reshape(-1)
+        return state_vec
+
     def _is_done(self):
-        for s in self.state:
+        state_vec = self._matrix2vec(self.state)
+        for s in state_vec:
             if s == 0:
                 return False
         
         return True
 
     def count_winner(self, player):
+        state_vec = self._matrix2vec(self.state)
         w = 0
         b = 0
-        for s in self.state:
+
+        for s in state_vec:
             if s == -1:
                 b += 1
             elif s == 1:
@@ -140,7 +145,7 @@ class Othello:
     def _is_legal_action(self, action):
 
         # 壁の追加（戻り値：壁付の盤面と、位置情報）
-        board, pos = self._add_wall(action)
+        board_vec, pos = self._add_wall(action)
 
         is_legal_action = False
         
@@ -155,7 +160,7 @@ class Othello:
             if search_pos < 0 or search_pos > self.max_range_index:
                 continue
 
-            num_opponent = self._linear_search(search_direction, board, search_pos)
+            num_opponent = self._linear_search(search_direction, board_vec, search_pos)
 
             if num_opponent > 0:
                 is_legal_action = True
@@ -164,7 +169,7 @@ class Othello:
 
         return is_legal_action
 
-    def _linear_search(self, search_direction, board, search_pos):
+    def _linear_search(self, search_direction, board_vec, search_pos):
         # 相手の駒のカウント初期化
         num_opponent = 0
 
@@ -172,7 +177,7 @@ class Othello:
         while True:
 
             # 相手の石なら
-            if board[search_pos] == -self.player:
+            if board_vec[search_pos] == -self.player:
                 num_opponent += 1
                 search_pos = search_pos + search_direction
                 # 範囲外のインデックスなら跳ばす
@@ -180,7 +185,7 @@ class Othello:
                     is_search_end_point = False
                     break
             # 自分の石なら
-            elif board[search_pos] == self.player:
+            elif board_vec[search_pos] == self.player:
                 is_search_end_point = True
                 break
             else:
@@ -198,27 +203,34 @@ class Othello:
         legal_actions = []
 
         for action in range(64):
-            if self.state[action] == 0:
+            x, y = self._action2xy(action)
+            # if self.state[action] == 0:
+            if self.state[x][y] == 0:
                 if self._is_legal_action(action):
                     legal_actions.append(action)
         
         return legal_actions
-   
+
+    def _action2xy(self, action):
+        x = action // self.CFG.lines
+        y = action %  self.CFG.lines
+        return x, y
+
     def _add_wall(self, action):
-        state = self.state
-        board = []
+        state_vec = self._matrix2vec(self.state)
+        board_vec = []
         pos = action
 
-        for i in range(len(state)):
+        for i in range(len(state_vec)):
             if i % self.CFG.lines == 0:
-                board.append(self.CFG.wall)
-                board.append(state[i])
+                board_vec.append(self.CFG.wall)
+                board_vec.append(state_vec[i])
                 if i <= action:
                     pos += 1
             else:
-                board.append(state[i])
+                board_vec.append(state_vec[i])
 
-        return board, pos
+        return board_vec, pos
 
     def _set_change_pos(self, pos, search_direction, num_opponent):
 
@@ -251,14 +263,9 @@ class Othello:
                 temp_change_pos_list[i] -= 7
             elif 64 <= pos <= 71:
                 temp_change_pos_list[i] -= 8
-
         
         for pos in temp_change_pos_list:
             self.change_pos_list.append(pos)
-
-        # for pos in self.change_pos_list:
-        #     self.state[pos] = self.player
-
 
     def draw_board(self):
         # https://qiita.com/secang0/items/1229212a37d8c9922901
@@ -267,7 +274,9 @@ class Othello:
         img = []
         counter = 1
 
-        for i in self.state:
+        state_vec = self._matrix2vec(self.state)
+
+        for i in state_vec:
             if i == 1:
                 j = [255, 255, 255] # white
             elif i == -1:
