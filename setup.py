b8d7082374fee124c07bfc3053f7ddbbf9f1a0c2
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-05-04 08:24:11 +0900

    Update setup.py
    bug fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='simple_othello', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='1.2.4',
+    version='1.2.5',
     license='MIT', 
     install_requires=[],
     author='Michiyasu Uchiyama',
